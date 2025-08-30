9de147daa7f81d2ed6fe1bca49133fff2fc7adcb
Guilherme Moreno <guilhermemoreno200@gmail.com>
2025-08-30 09:13:08 -0300

    migrations feita com sucesso

--- src/controllers/products-controllers.ts
+++ src/controllers/products-controllers.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from "express";
 class ProductsController {
   async index(req: Request, res: Response, next: NextFunction) {
     try {
+      return res.json({ message: "OK" });
     } catch (err) {
       next(err);
     }
